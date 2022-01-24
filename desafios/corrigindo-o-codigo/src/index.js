a86a3ba3f7fb7bdf84f300407be9451e06eeb082
Delio Marcos <deliomarcos.contato@gmail.com>
2022-01-24 00:47:15 -0300

    Challenge finished "Corrigindo o código"

---
+++ desafios/corrigindo-o-codigo/src/index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+
+const { v4: uuid } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body
+
+  const newRepository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(newRepository);
+
+  return response.status(201).json(newRepository);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  const updatedRepository = { 
+    ...repositories[repositoryIndex]
+  };
+
+  if (title)
+    updatedRepository.title = title;
+  
+  if (url)
+    updatedRepository.url = url;
+  
+  if (techs)
+    updatedRepository.techs = techs;
+
+  repositories[repositoryIndex] = updatedRepository;
+
+  return response.json(updatedRepository);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  repositories[repositoryIndex].likes += 1;
+
+  return response.status(201).json(repositories[repositoryIndex]);
+});
+
+module.exports = app;
