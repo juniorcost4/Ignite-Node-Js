2a4b6a65624af561bfebcc4c94b169b43f38c13d
Delio Marcos <deliomarcos.contato@gmail.com>
2022-01-26 01:17:13 -0300

    Aula: validando o cadastro de categoria;

--- aulas/Chapter II/iniciando-api/src/routes/categories.routes.ts
+++ aulas/Chapter II/iniciando-api/src/routes/categories.routes.ts
@@ -9,6 +9,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRouters.post("/", (request, response) => {
     const { name, description } = request.body;
 
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+        return response.status(400).json({ message: "Category already exists" });
+    }
+
     categoriesRepository.create({ name, description });
 
     response.status(201).send();
