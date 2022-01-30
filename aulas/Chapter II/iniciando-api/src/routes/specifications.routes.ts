f22a568cc60850ef5df2bee409eee86dcc1632c0
Delio Marcos <deliomarcos.contato@gmail.com>
2022-01-30 03:11:24 -0300

    Aula: Criando useCase de especificacao;

--- aulas/Chapter II/iniciando-api/src/routes/specifications.routes.ts
+++ aulas/Chapter II/iniciando-api/src/routes/specifications.routes.ts
@@ -1,19 +1,10 @@
-import { response, Router } from 'express';
-import { SpecificationsRepository } from '../modules/cars/repositories/implementations/SpecificationRepository';
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+import { Router } from 'express';
+import { createSpecificationcontroller } from '../modules/cars/useCases/createSpecification';
 
 const specificationsRoutes = Router();
 
-const specificationsRepository = new SpecificationsRepository();
-
 specificationsRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
-
-    const createSpecificationService = new CreateSpecificationService(specificationsRepository);
-
-    createSpecificationService.execute({ name, description });
-
-    return response.status(201).send();
+    return createSpecificationcontroller.handle(request, response);
 });
 
 export { specificationsRoutes };
