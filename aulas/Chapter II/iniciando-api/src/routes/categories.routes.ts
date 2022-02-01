7bf363c68efe4fc75359891877cd266929d658c8
Delio Marcos <deliomarcos.contato@gmail.com>
2022-01-31 23:24:58 -0300

    Aulas: Conhecendo o multer e Criando upload de arquivos

--- aulas/Chapter II/iniciando-api/src/routes/categories.routes.ts
+++ aulas/Chapter II/iniciando-api/src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import multer from 'multer';
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRouters = Router();
+
+const upload = multer({
+    dest: "./tmp"
+});
 
 categoriesRouters.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
@@ -11,6 +16,14 @@ categoriesRouters.post("/", (request, response) => {
 
 categoriesRouters.get("/", (request, response) => {
     return listCategoriesController.handle(request, response);
+});
+
+categoriesRouters.post("/import", upload.single("file"), (request, response) => {
+    const { file } = request;
+
+    console.log(file);
+
+    return response.send();
 });
 
 export { categoriesRouters };
