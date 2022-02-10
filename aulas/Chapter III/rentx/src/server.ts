27db464d6025ad7762b2d3538ef970340abb5987
Delio Marcos <deliomarcos.contato@gmail.com>
2022-02-10 19:11:07 -0300

    Corrigindo problema que nao deixava o servico do postgres startar;

--- aulas/Chapter III/rentx/src/server.ts
+++ aulas/Chapter III/rentx/src/server.ts
@@ -1,10 +1,10 @@
+import "reflect-metadata";
 import express from 'express';
 import swaggerUI from 'swagger-ui-express';
 
-import swaggerFile from './swagger.json';
-import { router } from './routes';
-
 import "./database";
+import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
 
